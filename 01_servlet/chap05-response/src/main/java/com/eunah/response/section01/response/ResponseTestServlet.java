9aa288c497656c18087d2f63ba59fb0f080f437f
EunahPae <eunahp86@gmail.com>
2024-06-05 13:52:27 +0900

    0605

--- 01_servlet/chap05-response/src/main/java/com/eunah/response/section01/response/ResponseTestServlet.java
+++ 01_servlet/chap05-response/src/main/java/com/eunah/response/section01/response/ResponseTestServlet.java
@@ -6,21 +6,37 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/response")
 public class ResponseTestServlet extends HttpServlet {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		/*
+		 * [ Servlet 의 역할 ] 1. 요청 받기 : HTTP method GET/POST 요청에 따라 parameter로 전달받은 데이터
+		 * 꺼낼 수 있다. 2. 비즈니스 로직 처리 = DB 접속 및 CRUD 로직 처리 -> Service를 호출하는 쪽으로 해결(MVC)한다.
+		 * 3. 응답하기 : 문자열로 동적인 웹(HTML 태그)페이지를 만들고 Stream을 이용해 내보낸다.
+		 */
+
+		PrintWriter out = response.getWriter();
+
+		System.out.println("default response type : " + response.getContentType());
+		System.out.println("default response encoding : " + response.getCharacterEncoding());
 		
-		 /*
-	       * [ Servlet 의 역할 ]
-	       * 1. 요청 받기 : HTTP method GET/POST 요청에 따라 parameter로 전달받은 데이터 꺼낼 수 있다.
-	       * 2. 비즈니스 로직 처리 = DB 접속 및 CRUD 로직 처리 -> Service를 호출하는 쪽으로 해결(MVC)한다.
-	       * 3. 응답하기 : 문자열로 동적인 웹(HTML 태그)페이지를 만들고 Stream을 이용해 내보낸다.
-	     */
+		response.setContentType("text/html; charset=UTF-8");
 		
-		
+		System.out.println("default response type : " + response.getContentType());
+		System.out.println("default response encoding : " + response.getCharacterEncoding());
+
+		StringBuilder responseBuilder = new StringBuilder();
+		responseBuilder.append("<!doctype html>\n").append("<html>\n").append("<head>\n").append("</head>\n")
+				.append("<body>\n").append("<h1>안녕 Servlet Response</h1>\n").append("</body>\n").append("</html>\n");
+
+		out.print(responseBuilder.toString());
+		out.close();
+
 	}
 
-	
 }
--- 01_servlet/chap05-response/src/main/webapp/index.jsp
+++ 01_servlet/chap05-response/src/main/webapp/index.jsp
@@ -1,12 +1,18 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Response</title>
 </head>
 <body>
+
+	<h1>Response</h1>
+
+	<ul>
+		<li><a href="response">Response 확인</a></li>
+	</ul>
 
 </body>
 </html>
